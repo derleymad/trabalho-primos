f297aaefe8c451aa31b85785365b5ab30629637c
Gabriel Lima <gabrielpk1@live.com>
2021-06-30 10:43:02 -0300

    adicionei uma  estruturas

--- main.c
+++ main.c
@@ -3,51 +3,82 @@
 #include <omp.h>
 #include <math.h>
 
+struct data
+{
+  int *primes;
+  int length;
+  int numThreads;
+  double time;
+};
+
+typedef struct data Data;
+
+#define MAX 10000000
+
 static int a[ ] = { [0 ... 10000000] = 1};
 
-double sieveSerial() {
+void setData(Data *data, int *a) {
+  data->primes = (int*)malloc(sizeof(int));
+  for(int i = 2; i < MAX; i++) {
+    if(a[i]) {
+      data->primes[data->length] = i;
+      data->length++;
+      int newSize = data->length + 1;
+      data->primes = (int*)realloc(data->primes, newSize * sizeof(Data));
+    }
+  }
+}
+
+void sieveSerial(Data *data) {
   double start = omp_get_wtime();
-  int n = 10000000;
-  int root = sqrt(n);
+  int root = sqrt(MAX);
+  int *a;
+  a = (int*)malloc(MAX*sizeof(int));
+
+  for(int i = 0; i < MAX; i++) {
+    a[i] = 1;
+  }
 
   for(int i = 2; i < root; i++) {
     if(a[i]) {
       a[i] = 1;
-      for(int j = 0; j < n; j++) {
+      for(int j = 0; j < MAX; j++) {
         int square = i * i;
         int index = square + (i * j);
-        if(index < n) a[index] = 0;
+        if(index < MAX) a[index] = 0;
         else break;
       }
     }
   }
-
-  /*for(int i = 2; i < n; i++) {
-    if(a[i]){
-      printf("%d\n", i);
-    }
-  }*/
+  setData(data, a);
+  free(a);
   double end = omp_get_wtime();
-
-  return end-start;
+  data->time = end-start;
 }
 
-double sieveThreads() {
+void sieveThreads(Data *data) {
   double start = omp_get_wtime();
+  int *a;
+  a = (int*)malloc(MAX*sizeof(int));
 
-  #pragma omp parallel num_threads(4)
+  int numThreads;
+  #pragma omp parallel shared(a)
   {
-    int n = 10000000;
-    int root = sqrt(n);
+    data->numThreads = omp_get_num_threads();
+    int root = sqrt(MAX);
+    #pragma omp for
+      for(int i = 0; i < MAX; i++) {
+        a[i] = 1;
+      }
 
     #pragma omp for
       for(int i = 2; i < root; i++) {
         if(a[i]) {
           a[i] = 1;
-          for(int j = 0; j < n; j++) {
-            int square = i * i;
+          int square = i * i;
+          for(int j = 0; j < MAX; j++) {
             int index = square + (i * j);
-            if(index < n) a[index] = 0;
+            if(index < MAX) a[index] = 0;
             else break;
           }
         }
@@ -59,21 +90,28 @@ double sieveThreads() {
       }
     }*/
   }
-
+  setData(data, a);
+  free(a);
   double end = omp_get_wtime();
-
-  return end-start;
+  data->time = end-start;
 }
 
 void main() {
-  double timeSerial = sieveSerial();
-  double timeThreads = sieveThreads();
 
-  double speedup = timeSerial/timeThreads;
-  double efficiency = timeSerial/(4*timeThreads);
+  Data serial, parallel;
+  serial.length = 0;
+  serial.numThreads = 1;
+  parallel.length = 0;
+  parallel.numThreads = 1;
+  sieveSerial(&serial);
+  sieveThreads(&parallel);
+  printf("time serial: %lf\ntime parallel: %lf", serial.time, parallel.time);
+  printf("\nThread's number:\n\tSerial-%d\n\tParallel-%d\n", serial.numThreads, parallel.numThreads);
+  // double speedup = timeSerial/timeThreads;
+  // double efficiency = timeSerial/(4*timeThreads);
 
-  printf("Time Serial: %f\n", timeSerial);
-  printf("Time Parallel: %f\n", timeThreads);
-  printf("Speedup: %f\n", speedup);
-  printf("Efficiency with 4 theads: %f\n", efficiency);
+  // printf("Time Serial: %f\n", timeSerial);
+  // printf("Time Parallel: %f\n", timeThreads);
+  // printf("Speedup: %f\n", speedup);
+  // printf("Efficiency with 4 theads: %f\n", efficiency);
 }
